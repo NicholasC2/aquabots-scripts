5de1a564c73c91bedeafe0fbe24f2d1d32b91977
NicholasC2 <nicholasc2@proton.me>
2026-08-04 13:13:10 +1200

    update i think

--- controller2bot.py
+++ controller2bot.py
@@ -1,8 +1,13 @@
 import pygame
 import serial
 import serial.tools.list_ports
+import time
 
 BAUD = 9600
+PORT = None
+SER = None
+
+MOTOR_SPEED = 255
 
 pygame.init()
 pygame.joystick.init()
@@ -19,25 +24,51 @@ controller.init()
 print(f"Controller Name: {controller.get_name()}")
 print(f"Buttons: {controller.get_numbuttons()}, Axis: {controller.get_numaxes()}")
 
-# for port in serial.tools.list_ports.comports():
-#     print(port.device)
+for port in serial.tools.list_ports.comports():
+    try:
+        ser = serial.Serial(port.device, BAUD, timeout=1)
+        PORT = port.device
+        print(f"Connected to {port.device}, {port.manufacturer} - {port.product}")
+    except Exception as e:
+        print(f"coulnt not connect to {port.device}, {port.manufacturer} - {port.product}: {e}")
+        continue
 
-clock = pygame.time.Clock()
+if not PORT:
+    print(f"no port found")
+    pygame.quit()
+    exit()
 
-while True:
-    for event in pygame.event.get():
-        if(event.type == pygame.QUIT):
-            exit()
+def send_command(motor_index, speed):
+    if ser and ser.is_open:
+        command = f"{motor_index},{int(speed * MOTOR_SPEED)}\n"
+        ser.write(command.encode())
+try:
+    while True:
+        for event in pygame.event.get():
+            if(event.type == pygame.QUIT):
+                exit()
 
-    lh = controller.get_axis(0)
-    lv = controller.get_axis(1)
+        lh = controller.get_axis(0)
+        if abs(lh) < 0.15:
+            lh = 0
+        lv = -controller.get_axis(1)
+        if abs(lv) < 0.15:
+            lv = 0
 
-    rh = controller.get_axis(3)
-    rv = controller.get_axis(4)
+        rh = controller.get_axis(3)
+        if abs(rh) < 0.15:
+            rh = 0
+        rv = -controller.get_axis(4)
+        if abs(rv) < 0.15:
+            rv = 0
 
-    lt = controller.get_axis(2)
-    rt = controller.get_axis(5)
+        lt = controller.get_axis(2)
+        rt = (controller.get_axis(5) + 1) / 2
 
-    print(f"left stick\nhorizontal: {lh}\nvertical: {lv}\nright stick\nhorizontal: {rh}\nvertical: {rv}\nlt: {lt}\nrt: {rt}")
+        send_command(1, rt)
+        time.sleep(1)
 
-    clock.tick(60)
+finally:
+    if ser and ser.is_open:
+        ser.close()
+    pygame.quit()
